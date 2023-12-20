07edc12a4528a89cb0fcd9c5cf1fc124b7cf3d5a
BootstrapFootkicker <ksealy93@gmail.com>
2023-12-20 15:53:24 -0500

    Auto stash before revert of "Info Tab"

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // eslint-disable-next-line no-unused-vars
 import App from "./components/App.jsx";
+import {
+  faGraduationCap,
+  faBriefcase,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./index.css";
 import PersonalDetails from "./components/PersonalDetails.jsx";
 import EducationForm from "./components/EducationForm.jsx";
 import ExperienceForm from "./components/ExperienceForm.jsx";
+import InfoTab from "./components/InfoTab.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -13,5 +19,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <PersonalDetails />
     <EducationForm />
     <ExperienceForm />
+    <InfoTab sectionName={"Education"} icon={faGraduationCap} />
+    <InfoTab sectionName={"Experience"} icon={faBriefcase} />
   </React.StrictMode>,
 );
